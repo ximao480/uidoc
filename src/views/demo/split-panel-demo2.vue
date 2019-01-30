6fd28894a77d811396c6c5726c799415b21fb8d3
qubingnian <qu.bn@burgeon.cn>
2019-01-30 11:45:58 +0800

    新增SplitPanel组件

---
+++ src/views/demo/split-panel-demo2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <span>固定宽度面板</span>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {};
+    },
+    name: '',
+    components: {},
+    methods: {},
+    watch: {},
+    computed: {}
+  };
+</script>
+
