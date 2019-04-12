ba3cc35e1eeb97796f9d200d0c0aa3060f2771dd
xia.hj <xia.hj@burgeon.cn>
2019-04-12 10:22:52 +0800

    增加ImageUpload文件

---
+++ src/code/ImageUpload.js
@@ -0,0 +1,36 @@
+let code = {};
+
+code.ImageUpload = `
+<template>
+    <ImageUpload
+            :dataitem="dataitem"
+            @uploadFileChange="uploadFileChange"
+            @deleteImg="deleteImg"
+            @uploadFileChangeSuccess="uploadFileChangeSuccess"
+            @uploadFileChangeOnerror="uploadFileChangeOnerror"
+            @uploadFileChangeOnload="uploadFileChangeOnload"
+            @uploadFileChangeOnloadstart="uploadFileChangeOnloadstart"
+            @uploadFileChangeOnloadend="uploadFileChangeOnloadend"
+    ></ImageUpload>
+</template>
+<script>
+    export default {
+        dataitem: {
+              url: '/upload', 
+              sendData:{}, 
+              width: 200,
+              height: 200,
+              name: '图片',
+              readonly: false,
+              valuedata: [
+                { NAME: 'image.jpg', URL:1.jpg') }
+              ]
+            }
+    }
+</script>
+`;
+
+
+
+
+export default code;
