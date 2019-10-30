204b9ecae01044282d771ba329da1f6f2acc7fde
xia.hj <xia.hj@burgeon.cn>
2019-10-30 13:56:05 +0800

    修改ImageUpload组件说明

--- src/code/ImageUpload.js
+++ src/code/ImageUpload.js
@@ -2,29 +2,36 @@ let code = {};
 
 code.ImageUpload = `
 <template>
-    <ImageUpload
-            :dataitem="dataitem"
-            @deleteImg="deleteImg"
-            @uploadFileChangeSuccess="uploadFileChangeSuccess"
-            @uploadFileChangeOnerror="uploadFileChangeOnerror"
-            @uploadFileChangeOnload="uploadFileChangeOnload"
-            @uploadFileChangeOnloadstart="uploadFileChangeOnloadstart"
-            @uploadFileChangeOnloadend="uploadFileChangeOnloadend"
-    ></ImageUpload>
+<ImageUpload
+   :dataitem="dataitem"
+></ImageUpload>
 </template>
 <script>
     export default {
-        dataitem: {
-              url: '/upload', 
-              sendData:{}, 
-              width: 200,
-              height: 200,
-              name: '图片',
-              readonly: false,
-              valuedata: [
-                { NAME: 'image.jpg', URL:1.jpg') }
-              ]
-            }
+      data() {
+        return {
+          dataitem: {
+            url: '', // 上传地址
+            sendData: {}, // 上传参数
+            width: 250,
+            height: 250,
+            readonly: false, 
+            valuedata: [
+                 {
+                      NAME: '1.jpg',
+                      URL:
+                        'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1561364661&di=766d38b55df12b23516f195942aa0055&src=http://img.zhiribao.com/upload/images/201607/6/6b0152a5b29f309f3f92f52adc6cd017eae73133.jpg'
+                    },
+                     {
+                      NAME: '2.jpg',
+                      URL:
+                        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561374887838&di=8027dfa1209bc0033b5168958c4608b2&imgtype=0&src=http%3A%2F%2Fimg.sccnn.com%2Fbimg%2F337%2F31662.jpg'
+                    },
+            ],
+            name: '上传'  //点击上传按钮名称
+          }
+        };
+      },
     }
 </script>
 `;
--- src/views/components/ImageUpload.vue
+++ src/views/components/ImageUpload.vue
@@ -8,9 +8,9 @@
             <Demo title="水平分割">
                 <div slot="demo">
                     <div class="demo-split-panel">
-                        <ImageUpload
-                        :dataitem="dataitem"
-                        ></ImageUpload>
+                         <ImageUpload
+                        :dataitem="ImageUploadData"
+                          ></ImageUpload>
                     </div>
                 </div>
                 <div slot="desc">
@@ -184,7 +184,27 @@
     data () {
       return {
         code: Code,
-        dataitem:{}
+        dataitem:{},
+        ImageUploadData: {
+                url: '', // 上传地址
+                sendData: {}, // 上传参数
+                width: 200,
+                height: 200,
+                readonly: true,
+                valuedata: [
+                    {
+                        NAME: '1.jpg',
+                        URL:
+                            'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1561364661&di=766d38b55df12b23516f195942aa0055&src=http://img.zhiribao.com/upload/images/201607/6/6b0152a5b29f309f3f92f52adc6cd017eae73133.jpg'
+                        },
+                        {
+                        NAME: '2.jpg',
+                        URL:
+                            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561374887838&di=8027dfa1209bc0033b5168958c4608b2&imgtype=0&src=http%3A%2F%2Fimg.sccnn.com%2Fbimg%2F337%2F31662.jpg'
+                        }
+                ],
+                name: '上传'
+         }
      
       }
     },
