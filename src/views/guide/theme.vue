bc354fa6b784929726dc872db42dfa4c18f0193f
843748862@qq.com <843748862@qq.com>
2020-10-20 10:53:18 +0800

    主题文件地址更新

--- src/views/guide/theme.vue
+++ src/views/guide/theme.vue
@@ -17,7 +17,7 @@
             <p>首先在项目中先建一个目录，比如 <code>my-theme</code>，然后在 my-theme 下建立一个 less 文件 <code>index.less</code>，并写入下面内容：</p>
             <i-code lang="auto" bg>{{ code.theme.less }}</i-code>
             <p>
-                完整的变量列表可以查看 <a href="https://gitee.com/platform_frame_part_front_end/burgeon-UI/blob/master/src/styles/theme/theme.less" target="_blank">默认样式变量</a>。
+                完整的变量列表可以查看 <a href="https://gitee.com/platform_frame_part_front_end/burgeon-UI/blob/ark-ui/src/styles/theme/theme.less" target="_blank">默认样式变量</a>。
             </p>
             <p>然后在入口文件 <code>main.js</code> 内导入这个 less 文件即可：</p>
             <i-code lang="auto" bg>{{ code.theme.lessImport }}</i-code>
