d2db3f160790eace67645918ed22a5550402c28b
chenzihao <278199196@qq.com>
2021-04-09 14:39:45 +0800

    版本号没改

--- src/views/guide/update.vue
+++ src/views/guide/update.vue
@@ -24,7 +24,7 @@
         <h1>更新日志</h1>
         <article class="doc-update" >
             <TimelineItem pending>
-                <inAnchor title="1.1.21" h2></inAnchor>
+                <inAnchor title="1.1.23" h2></inAnchor>
                 <p>
                     <code>2021-04-8</code>
                 </p>
