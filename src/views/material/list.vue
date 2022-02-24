19b090f26e4b37ed2b62ae920128dfb42591f0f3
xc9010 <xuchen199010@gmail.com>
2022-02-24 13:47:47 +0800

    update-1.2.17

--- src/views/material/list.vue
+++ src/views/material/list.vue
@@ -179,6 +179,31 @@ export default {
         "email": "278199196@qq.com"
       }, {"username": "xuchen199010", "email": "xc9010@qq.com"}]
     }, {
+      "name": "@syman/tree-charts",
+      "scope": "syman",
+      "version": "0.1.2",
+      "description": "## Project setup ``` npm install ```",
+      "keywords": ["name=arkdoc&type=2"],
+      "date": "2021-12-29T07:50:42.730Z",
+      "links": {"npm": "https://www.npmjs.com/package/%40syman%2Ftree-charts"},
+      "publisher": {"username": "xuchen199010", "email": "xc9010@qq.com"},
+      "maintainers": [{"username": "huai.yang", "email": "hycool.happy@163.com"}, {
+        "username": "shiliuyuea",
+        "email": "liuyuewo@126.com"
+      }, {"username": "qubingnian", "email": "1752711471@qq.com"}, {
+        "username": "bi.j",
+        "email": "1285345902@qq.com"
+      }, {"username": "xia.hj", "email": "baizhi0425@163.com"}, {
+        "username": "ximao",
+        "email": "576747591@qq.com"
+      }, {"username": "xieshihua", "email": "843748862@qq.com"}, {
+        "username": "burgeon-developer",
+        "email": "huai.y@burgeon.cn"
+      }, {"username": "gqw", "email": "guo.qw@burgeon.cn"}, {
+        "username": "yiq",
+        "email": "278199196@qq.com"
+      }, {"username": "xuchen199010", "email": "xc9010@qq.com"}]
+    }, {
       "name": "@syman/bar-charts",
       "scope": "syman",
       "version": "0.1.1",
