c285e1fa5aba8e0135057f395f3f4b78dc0e9e53
liu.li <li.liu@burgeon.cn>
2022-06-01 14:23:00 +0800

    修改文档路径

--- src/views/cli/start.vue
+++ src/views/cli/start.vue
@@ -1,7 +1,7 @@
 <template>
   
   <i-article>
-    <iframe data-v-e561dc48="" src="https://unpkg.com/@syman/ark-cli/storybook-static/index.html?path=/story/%E5%BF%AB%E9%80%9F%E5%AE%89%E8%A3%85--page" seamless="seamless" width="100%" height="100%" frameborder="0" scrolling="auto" 
+    <iframe data-v-e561dc48="" src="https://oss.font.burgeononline.com/DOC/%40syman/ark-cli/index.html" seamless="seamless" width="100%" height="100%" frameborder="0" scrolling="auto" 
     id="iframename" name="iframename"
      style="height: 800px; 
     left: -0px;
